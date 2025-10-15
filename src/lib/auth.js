4c526ab523d7523674a01c33381d5f70df0749cb
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2025-10-15 11:10:02 -0700

    google credentials was for a desktop app. Updated for web app and configured app

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,10 +12,12 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
+        url: "https://accounts.google.com/o/oauth2/v2/auth",
         params: {
           prompt: "consent",
           access_type: "offline",
-          response_type: "code"
+          response_type: "code",
+          scope: "openid profile email"
         }
       }
     }),
